22fdc397198aa483791cb6545eb6d5ca1ec3effc
veectro <veectro@gmail.com>
2023-01-07 02:22:38 +0100

    chore: fix ui

--- src/dapp/contract.js
+++ src/dapp/contract.js
@@ -2,9 +2,10 @@ import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
 import Config from './config.json';
 import Web3 from 'web3';
 
+const fundAmount = Web3.utils.toWei("10", "ether");
+
 export default class Contract {
     constructor(network, callback) {
-
         let config = Config[network];
         this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
         this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
@@ -16,16 +17,17 @@ export default class Contract {
 
     initialize(callback) {
         this.web3.eth.getAccounts((error, accts) => {
-           
             this.owner = accts[0];
 
             let counter = 1;
-            
-            while(this.airlines.length < 5) {
+
+            while (this.airlines.length < 5) {
                 this.airlines.push(accts[counter++]);
             }
 
-            while(this.passengers.length < 5) {
+            this.fundAirline(this.airlines[3]);
+
+            while (this.passengers.length < 5) {
                 this.passengers.push(accts[counter++]);
             }
 
@@ -33,23 +35,80 @@ export default class Contract {
         });
     }
 
+    fundAirline(airlineAddress) {
+        let self = this;
+
+        console.log(`Funding airline with address ${airlineAddress} with amount ${fundAmount}`);
+        //self.flightSuretyApp.methods
+        //    .fund()
+        //    .send({from: airlineAddress, value: fundAmount}, (error, result) => {
+        //        console.log(error, result);
+        //    });
+        console.log(`Successful : Funding airline with address ${airlineAddress} with amount ${fundAmount}`);
+    }
+
     isOperational(callback) {
-       let self = this;
-       self.flightSuretyApp.methods
+        let self = this;
+        self.flightSuretyApp.methods
             .isOperational()
-            .call({ from: self.owner}, callback);
+            .call({from: self.owner}, callback);
+    }
+
+    buyInsurance(amount, flight, callback) {
+        let self = this;
+
+        let passengerAddress = self.passengers[3];
+
+        console.log(`Buy insurance with address ${passengerAddress} with amount ${amount}`);
+        try {
+            let payload = {
+                airline: self.airlines[3],
+                flight,
+                amount: self.web3.utils.toWei(amount, "ether").toString(),
+                timestamp: Math.floor(Date.now() / 1000),
+            };
+
+            self.flightSuretyApp.methods
+                .buyInsurance(payload.airline)
+                .send(
+                    {from: passengerAddress, value: payload.amount},
+                    (error, result) => {
+                        callback(error, result);
+                    }
+                );
+        } catch (error) {
+            console.log(error);
+        }
+        console.log(`Successful : Buy insurance with address ${passengerAddress} with amount ${amount}`);
+    }
+
+    withdraw(callback) {
+        let self = this;
+        let passengerAddress = self.passengers[3];
+        try {
+            self.flightSuretyApp.methods
+                .withdraw()
+                .send({from: passengerAddress}, (error, result) => {
+                    callback(error, result);
+                });
+            console.log(`Successful : Withdraw insurance with address ${passengerAddress}`);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     fetchFlightStatus(flight, callback) {
         let self = this;
         let payload = {
-            airline: self.airlines[0],
+            airline: self.airlines[3],
             flight: flight,
-            timestamp: Math.floor(Date.now() / 1000)
-        } 
+            timestamp: Math.floor(Date.now() / 1000),
+        };
+
         self.flightSuretyApp.methods
             .fetchFlightStatus(payload.airline, payload.flight, payload.timestamp)
-            .send({ from: self.owner}, (error, result) => {
+            .send({from: self.owner}, (error, result) => {
+                console.log(error, payload);
                 callback(error, payload);
             });
     }
--- src/dapp/index.js
+++ src/dapp/index.js
@@ -1,66 +1,127 @@
-import DOM from './dom';
-import Contract from './contract';
-import './flightsurety.css';
-
+import DOM from "./dom";
+import Contract from "./contract";
+import "./flightsurety.css";
 
 (async () => {
-
-    let result = null;
-
-    let contract = new Contract('localhost', () => {
-
-        // Read transaction
+    let contract = new Contract("localhost", () => {
         contract.isOperational((error, result) => {
             console.log(error, result);
-            display('Operational Status',
-                'Check if contract is operational',
-                [{
-                    label: 'Operational Status',
-                    error: error,
-                    value: result
-                }]);
+            display("Operational Status", "Check if contract is operational", [
+                { label: "Operational Status", error: error, value: result },
+            ]);
         });
 
+        DOM.elid("submit-oracle").addEventListener("click", () => {
+            let flight = DOM.elid("flight-number").value;
 
-        // User-submitted transaction
-        DOM.elid('submit-oracle').addEventListener('click', () => {
-            let flight = DOM.elid('flight-number').value;
-            // Write transaction
             contract.fetchFlightStatus(flight, (error, result) => {
-                display('Oracles',
-                    'Trigger oracles',
-                    [{
-                        label: 'Fetch Flight Status',
+                display("Oracles", "Trigger oracles", [
+                    {
+                        label: "Set Flight Status",
                         error: error,
-                        value: result.flight + ' ' + result.timestamp
-                    }]);
+                        value: result.flight + " " + result.timestamp,
+                    },
+                ]);
             });
-        })
+        });
 
+        DOM.elid("withdraw-credits").addEventListener("click", () => {
+            contract.withdraw((error, result) => {
+                console.log(error, result);
+            });
+        });
+
+        const display = (title, description, results) => {
+            let displayDiv = DOM.elid("display-wrapper");
+            let section = DOM.section();
+            section.appendChild(DOM.h4(title));
+            section.appendChild(DOM.h5(description));
+            results.map((result) => {
+                let row = section.appendChild(DOM.div({ className: "row" }));
+                row.appendChild(DOM.div({ className: "col-sm-4 field" }, result.label));
+                row.appendChild(
+                    DOM.div(
+                        { className: "col-sm-8 field-value" },
+                        result.error ? String(result.error) : String(result.value)
+                    )
+                );
+                section.appendChild(row);
+            });
+            displayDiv.append(section);
+        };
+
+        const buyInsurance = () => {
+            const flight = DOM.elid("flight-number").value;
+            const amount = DOM.elid("insurance-amount").value;
+            console.log(flight, amount);
+
+            try {
+                contract.buyInsurance(amount, flight, (error, result) => {
+                    console.log("Insurance purchased with", amount);
+                    display(
+                        "Oracles",
+                        "Trigger oracles",
+                        [
+                            {
+                                label: "Assurance Detail",
+                                error: error,
+                                value:
+                                    "Flight Name: " +
+                                    flight +
+                                    " | Assurance Paid: " +
+                                    amount +
+                                    " ether" +
+                                    " | Paid on Delay: " +
+                                    amount * 1.5 +
+                                    " ETH",
+                            },
+                        ],
+                        "display-flight",
+                        "display-detail"
+                    );
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        const selectFlight = (event) => {
+            DOM.elid("dropdownMenuLink").textContent = event.target.text;
+            DOM.elid("flight-number").value = event.target.text;
+        };
+
+        const renderFlights = async () => {
+            try {
+                const flights = [
+                    { id: 1, name: "LAH3912" },
+                    { id: 2, name: "RYA2141" },
+                    { id: 3, name: "KLM1231" }
+                ];
+
+                flights.forEach((f) => {
+                    const option = DOM.a(
+                        {
+                            className: `dropdown-item`
+                        },
+                        f.name
+                    );
+
+                    option.addEventListener("click", selectFlight);
+
+                    DOM.elid("flights").appendChild(option);
+                });
+
+                DOM.elid("buy-flight-insurance").addEventListener(
+                    "click",
+                    buyInsurance
+                );
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        DOM.elid("connected-address").textContent = contract.passengers[3];
+
+        renderFlights();
     });
-
-
 })();
-
-
-function display(title, description, results) {
-    let displayDiv = DOM.elid("display-wrapper");
-    let section = DOM.section();
-    section.appendChild(DOM.h4(title));
-    section.appendChild(DOM.h5(description));
-    results.map((result) => {
-        let row = section.appendChild(DOM.div({className: 'row'}));
-        row.appendChild(DOM.div({className: 'col-sm-4 field'}, result.label));
-        row.appendChild(DOM.div({className: 'col-sm-8 field-value'}, result.error ? String(result.error) : String(result.value)));
-        section.appendChild(row);
-    })
-    displayDiv.append(section);
-
-}
-
-
-
-
-
-
-
